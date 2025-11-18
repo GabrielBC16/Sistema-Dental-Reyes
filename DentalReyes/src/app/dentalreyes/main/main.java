51e332bbf74483d9307cffbe237555053c4f8b27
Gabriel Barrantes <gabrielf.barrantes@gmail.com>
2025-11-17 23:17:51 -0500

    Subida inicial: Arquitectura modular Dental Reyes completa

---
+++ DentalReyes/src/app/dentalreyes/main/main.java
@@ -0,0 +1,12 @@
+package app.dentalreyes.main;
+
+import app.dentalreyes.seguridad.vistas.IU_AutenticarUsuario;
+
+public class main {
+    public static void main(String[] args) {
+        
+        IU_AutenticarUsuario loginForm = new IU_AutenticarUsuario();
+        loginForm.setVisible(true);
+        
+    }
+}
